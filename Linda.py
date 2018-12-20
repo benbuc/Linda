698052ae0f511bde7dd320d41b35cc1853a336a1
Benito Buchheim <github.benito.2017@buchheims.de>
2018-12-20 17:09:47 +0100

    implemented loadServices and DATAPATH constant

--- Linda.py
+++ Linda.py
@@ -1,15 +1,41 @@
+# Author: Benito Buchheim
+
 from LindaService import *
+import pickle
+import os
+import os.path
+
+DATAPATH = "data/"
 
 class Linda(object):
 
     def __init__(self):
-        pass
+        self.services = []
+
+    def loadServices(self, datapath):
+        # iterate through all services
+        for filename in os.listdir(datapath):
+            if not filename.endswith(".lise"):
+                continue
+
+            # load the service and add to services
+            with open(os.path.join(datapath, filename), 'r') as f:
+                self.services.append(pickle.load(f))
 
     def checkAll(self):
-        return 0
+        # checks through all services and triggers
+        for service in self.services:
+            self.check(service)
 
     def check(self, service):
-        pass
+        service.check()
+
+    def addService(self, service):
+        self.services.append(service)
+
+    def removeService(self, service):
+        self.services.remove(service)
+
 
 if __name__ == "__main__":
-    Linda().checkAll()
+    pass
