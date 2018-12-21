68376ce27e8b1290faa274562b11ff75832cfa31
Benito Buchheim <github.benito.2017@buchheims.de>
2018-12-21 19:15:52 +0100

    added some remove functionality

--- Linda.py
+++ Linda.py
@@ -9,17 +9,24 @@ DATAPATH = "data/"
 
 class Linda(object):
 
-    def __init__(self):
+    def __init__(self, datapath):
         self.services = []
+        self.datapath = datapath
 
-    def loadServices(self, datapath):
-        # iterate through all services
-        for filename in os.listdir(datapath):
+    def loadServices(self):
+        self.services = []
+        # check if datapath exists
+        if not os.path.exists(self.datapath):
+            print("loadServices: datapath does not exist")
+            return
+
+        # iterate through all services and add to list
+        for filename in os.listdir(self.datapath):
             if not filename.endswith(".lise"):
                 continue
 
             # load the service and add to services
-            with open(os.path.join(datapath, filename), 'r') as f:
+            with open(os.path.join(self.datapath, filename), 'r') as f:
                 self.services.append(pickle.load(f))
 
     def checkAll(self):
@@ -30,12 +37,20 @@ class Linda(object):
     def check(self, service):
         service.check()
 
-    def addService(self, service):
-        self.services.append(service)
+    def removeServiceWithName(self, serviceName):
+        """Remove all services with given name."""
+        if not os.path.exists(self.datapath):
+            print("removeServiceWithName: datapath does not exist")
+            return
+        
+        for filename in os.listdir(self.datapath):
+            if not filename.endswith(".lise"):
+                continue
 
-    def removeService(self, service):
-        self.services.remove(service)
-
+            with open(os.path.join(self.datapath, filename), 'r') as f:
+                if pickle.load(f).name == serviceName:
+                    os.remove(os.path.join(self.datapath, filename))
 
 if __name__ == "__main__":
-    pass
+    linda = Linda(DATAPATH)
+    
