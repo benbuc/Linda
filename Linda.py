61cf813358d99bff7a155f5576cb48ee2225f872
Benito Buchheim <github.benito.2017@buchheims.de>
2018-12-22 15:02:23 +0100

    added some basic logging to Linda.py

--- Linda.py
+++ Linda.py
@@ -4,21 +4,49 @@ from LindaService import *
 import pickle
 import os
 import os.path
+import configparser
+import logging
 
 DATAPATH = "data/"
+CONFIGFILE = "lindaconfig.ini"
 
 class Linda(object):
 
-    def __init__(self, datapath):
+    def __init__(self, configfile):
+        self.log = logging.getLogger()
+        self.logHandler = logging.StreamHandler()
+        
+        self.logFormatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
+        self.logHandler.setFormatter(self.logFormatter)
+        self.log.addHandler(self.logHandler)
+
+        self.log.setLevel(logging.DEBUG)
+
+        self.log.info("Linda initializing")
+
         self.services = []
-        self.datapath = datapath
+        self.configfile = configfile
+
+        self.readConfig()
+
+        self.loadServices()
+
+    def readConfig(self):
+        self.log.debug("Reading configuration files")
+        config = configparser.ConfigParser()
+        config.read(self.configfile)
+
+        # get datapath config
+        self.datapath = config.get("DEFAULT", "datapath", fallback="data")
+        # check if directory exists and create if not
+        self.log.debug("Checking if datapath exists")
+        if not os.path.exists(self.datapath):
+            self.log.debug("Creating directory for datapath")
+            os.mkdir(self.datapath)
 
     def loadServices(self):
+        self.log.debug("Loading services from datapath")
         self.services = []
-        # check if datapath exists
-        if not os.path.exists(self.datapath):
-            print("loadServices: datapath does not exist")
-            return
 
         # iterate through all services and add to list
         for filename in os.listdir(self.datapath):
@@ -26,11 +54,13 @@ class Linda(object):
                 continue
 
             # load the service and add to services
+            self.log.debug("Adding service: %s", filename)
             with open(os.path.join(self.datapath, filename), 'r') as f:
                 self.services.append(pickle.load(f))
 
     def checkAll(self):
         # checks through all services and triggers
+        self.log.debug("Checking all services")
         for service in self.services:
             self.check(service)
 
@@ -39,9 +69,7 @@ class Linda(object):
 
     def removeServiceWithName(self, serviceName):
         """Remove all services with given name."""
-        if not os.path.exists(self.datapath):
-            print("removeServiceWithName: datapath does not exist")
-            return
+        self.log.debug("Removing service with name")
         
         for filename in os.listdir(self.datapath):
             if not filename.endswith(".lise"):
@@ -52,5 +80,5 @@ class Linda(object):
                     os.remove(os.path.join(self.datapath, filename))
 
 if __name__ == "__main__":
-    linda = Linda(DATAPATH)
+    linda = Linda(CONFIGFILE)
     
